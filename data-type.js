4699e066b1be3a745cd669320bde2d5fad2f37bf
Sobuj <sobujkumar424@gmail.com>
2022-02-25 15:59:34 +0600

    data-type

---
+++ data-type.js
@@ -0,0 +1,16 @@
+// Data types 7 aitems
+// primitive data types
+/* 1. number
+2. string
+3. boolean
+4. undefined
+5. null
+6. object ---(non-object)
+7. symbol
+ */
+// example
+let a = 'Hello';
+let b = a;
+// console.log(a, b);
+let a = 'Hi';
+console.log(a, b);
